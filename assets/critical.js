b056d53a8eb1ddf5fdfd70891d7ee706d46d2cb1
bemozi <211243872+bemozi@users.noreply.github.com>
2025-12-16 23:31:35 +0100

    Update critical.js
    Signed-off-by: bemozi <211243872+bemozi@users.noreply.github.com>

--- assets/critical.js
+++ assets/critical.js
@@ -2,13 +2,29 @@ const controller = new AbortController();
 const {signal} = controller;
 
 addEventListener('load', event => {
-	
+	dialog.open || dialog.showModal();
 }, {signal});
 
 const dialog = document.getElementsByTagName('dialog')[0];
 
+addEventListener('pointerdown', event => {
+	
+}, {signal});
+
+addEventListener('pointerup', event => {
+	
+}, {signal});
+
 addEventListener('pointermove', event => {
-	dialog.open || dialog.showModal();
+	
+}, {signal});
+
+addEventListener('focus', event => {
+	
+}, {signal});
+
+addEventListener('visibilitychange', event => {
+	
 }, {signal});
 
 addEventListener('beforeunload', event => {
