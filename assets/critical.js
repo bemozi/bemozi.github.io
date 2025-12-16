0e435f17b3b83ca657787971b8fd3ccbc47cb54c
bemozi <211243872+bemozi@users.noreply.github.com>
2025-12-16 14:26:06 +0100

    Update critical.js
    Signed-off-by: bemozi <211243872+bemozi@users.noreply.github.com>

--- assets/critical.js
+++ assets/critical.js
@@ -1,3 +1,10 @@
-onload = event => {
+const controller = new AbortController();
+const {signal} = controller;
+
+document.addEventListener('load', event => {
+	
+}, {signal});
+
+document.addEventListener('beforeunload', event => {
 	document.getElementsByTagName('dialog')[0].showModal();
-};
+}, {signal});
