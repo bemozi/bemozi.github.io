04ba21bf44515ef047232558876e8b3f43e2a660
bemozi <211243872+bemozi@users.noreply.github.com>
2026-02-05 09:37:33 +0100

    Update critical.js
    Signed-off-by: bemozi <211243872+bemozi@users.noreply.github.com>

--- assets/critical.js
+++ assets/critical.js
@@ -246,6 +246,20 @@
 		},
 	}, view: {
 		frame: async (z, value) => {
+			head.replaceChildren(...head.querySelectorAll(`
+				meta[charset],
+				meta[http-equiv],
+				meta[name="referrer"],
+				title,
+				meta[name="viewport"],
+				base,
+				link[rel="stylesheet"],
+				meta[name="theme-color"],
+				link[rel="manifest"],
+				link[rel="icon"],
+				link[rel="apple-touch-icon"],
+				script:not([type="application/ld+json"])
+			`));
 			document.body.style.visibility = 'hidden';
 			const head = document.head;
 			const stylesheet = head.querySelector('link[href="landing.css"]');
@@ -280,20 +294,6 @@
 				</div>
 				<footer id="footer"></footer>
 			`, null, true);
-			head.replaceChildren(...head.querySelectorAll(`
-				meta[charset],
-				meta[http-equiv],
-				meta[name="referrer"],
-				title,
-				meta[name="viewport"],
-				base,
-				link[rel="stylesheet"],
-				meta[name="theme-color"],
-				link[rel="manifest"],
-				link[rel="icon"],
-				link[rel="apple-touch-icon"],
-				script:not([type="application/ld+json"])
-			`));
 		},
 	},
 }});
