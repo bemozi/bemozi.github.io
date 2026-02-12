425498393f5a82136c3a820d69be8b7b2bbc3bf7
bemozi <211243872+bemozi@users.noreply.github.com>
2026-02-12 10:30:12 +0100

    Update critical.js
    Signed-off-by: bemozi <211243872+bemozi@users.noreply.github.com>

--- assets/critical.js
+++ assets/critical.js
@@ -286,6 +286,21 @@
 						<button class="icon-btn toolbar-btn" id="undoBtn" disabled title="Undo">
 							<svg viewBox="0 0 24 24"><path d="M9 14L4 9l5-5"/><path d="M20 20v-7a4 4 0 0 0-4-4H4"/></svg>
 						</button>
+						<button class="icon-btn toolbar-btn" id="undoBtn" disabled title="Undo">
+							<svg viewBox="0 0 24 24"><path d="M9 14L4 9l5-5"/><path d="M20 20v-7a4 4 0 0 0-4-4H4"/></svg>
+						</button>
+						<button class="icon-btn toolbar-btn" id="undoBtn" disabled title="Undo">
+							<svg viewBox="0 0 24 24"><path d="M9 14L4 9l5-5"/><path d="M20 20v-7a4 4 0 0 0-4-4H4"/></svg>
+						</button>
+						<button class="icon-btn toolbar-btn" id="undoBtn" disabled title="Undo">
+							<svg viewBox="0 0 24 24"><path d="M9 14L4 9l5-5"/><path d="M20 20v-7a4 4 0 0 0-4-4H4"/></svg>
+						</button>
+						<button class="icon-btn toolbar-btn" id="undoBtn" disabled title="Undo">
+							<svg viewBox="0 0 24 24"><path d="M9 14L4 9l5-5"/><path d="M20 20v-7a4 4 0 0 0-4-4H4"/></svg>
+						</button>
+						<button class="icon-btn toolbar-btn" id="undoBtn" disabled title="Undo">
+							<svg viewBox="0 0 24 24"><path d="M9 14L4 9l5-5"/><path d="M20 20v-7a4 4 0 0 0-4-4H4"/></svg>
+						</button>
 						<span></span>
 						<button class="icon-btn toolbar-btn" id="redoBtn" disabled title="Redo">
 							<svg viewBox="0 0 24 24"><path d="M15 14l5-5-5-5"/><path d="M4 20v-7a4 4 0 0 1 4-4h12"/></svg>
