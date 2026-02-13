487a8c28dca7cb67c46a5fc5338d7f46d7b4f1b2
bemozi <211243872+bemozi@users.noreply.github.com>
2026-02-13 11:01:30 +0100

    Update critical.js
    Signed-off-by: bemozi <211243872+bemozi@users.noreply.github.com>

--- assets/critical.js
+++ assets/critical.js
@@ -342,23 +342,9 @@
 					</div>
 					<button id="explore_toggle" title="Explore">
 						<svg viewBox="0 0 24 24">
-							<path d="M9 20H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2v2"></path>
-							<line x1="15" y1="14" x2="21" y2="14"></line>
-							<line x1="15" y1="18" x2="21" y2="18"></line>
-							<line x1="15" y1="22" x2="19" y2="22"></line>
-						</svg>
-					</button>
-					<button id="explore_toggle" title="Explore">
-						<svg viewBox="0 0 24 24">
 							<path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
 							<line x1="8" y1="12" x2="16" y2="12"></line>
 							<line x1="8" y1="16" x2="13" y2="16"></line>
-						</svg>
-					</button>
-					<button id="explore_toggle" title="Explore">
-						<svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="square">
-							<path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
-							<line x1="8" y1="13" x2="16" y2="13"></line>
 						</svg>
 					</button>
 					<button id="prefBtn" class="btn">
