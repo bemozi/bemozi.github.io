d7700843cef26b2c0ddeb662fa6cd36d30ec7dc4
bemozi <211243872+bemozi@users.noreply.github.com>
2026-02-13 09:25:13 +0100

    Update critical.js
    Signed-off-by: bemozi <211243872+bemozi@users.noreply.github.com>

--- assets/critical.js
+++ assets/critical.js
@@ -347,6 +347,13 @@
 							<path d="m21 21-1.9-1.9"></path>
 						</svg>
 					</button>
+					<button id="explore_toggle" title="Explore">
+						<svg viewBox="0 0 24 24">
+							<path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+							<line x1="8" y1="12" x2="16" y2="12"></line>
+							<line x1="8" y1="16" x2="13" y2="16"></line>
+						</svg>
+					</button>
 					<button id="prefBtn" class="btn">
 						<svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="square">
 							<path d="M4 6h16"/>
